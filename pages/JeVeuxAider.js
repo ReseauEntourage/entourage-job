5fd031dbdf7fea2c512972648ed9b9224ba2f824
Hospice <johannhospice.dev@gmail.com>
2019-08-02 18:57:14 +0200

    Updating helping page

--- pages/JeVeuxAider.js
+++ pages/JeVeuxAider.js
@@ -1,5 +1,82 @@
 const JeVeuxAider = () => (
-  <div className="uk-text-lead uk-text-center">Je veux aider</div>
+  <div>
+    <div className="uk-section uk-section-default">
+      <div className="uk-container">
+        <div data-uk-grid>
+          <div className="uk-width-1-2" id="help">
+            <div className="uk-margin-right">
+              <h1>Vous souhaitez <span className="uk-text-primary">aider</span> ?</h1>
+            </div>
+          </div>
+          <div className="uk-width-1-2">
+            <div className="uk-card uk-card-default uk-card-body" data-uk-grid>
+              <div className="uk-width-2-3">
+                <h3 className="uk-text-bold">Vous connaissez une personne en difficulté ? <span className="uk-text-primary">Entourage vous accompagne</span> dans les démarches</h3>
+                <p>Vous n'êtes pas seul! Entourage vous donne les outils pour soutenir efficacement une personne danssa recherche professionnelle et dans sa prise de poste</p>
+              </div>
+              <div className="uk-width-1-3">
+                <div>
+              </div>
+                <img src="/static/img/help_1.png" width="100%"/>
+              </div>
+            </div>
+            <div className="uk-card uk-card-default uk-card-body" data-uk-grid>
+              <div className="uk-width-2-3">
+                <h3 className="uk-text-bold">Vous connaissez une personne en difficulté ? <span className="uk-text-primary">Entourage vous accompagne</span> dans les démarches</h3>
+                <p>Vous n'êtes pas seul! Entourage vous donne les outils pour soutenir efficacement une personne danssa recherche professionnelle et dans sa prise de poste</p>
+              </div>
+              <div className="uk-width-1-3">
+                <img src="/static/img/help_2.png" width="100%"/>
+              </div>
+            </div>
+            <div className="uk-card uk-card-default uk-card-body" data-uk-grid>
+              <div className="uk-width-2-3">
+                <h3 className="uk-text-bold">Vous connaissez une personne en difficulté ? <span className="uk-text-primary">Entourage vous accompagne</span> dans les démarches</h3>
+                <p>Vous n'êtes pas seul! Entourage vous donne les outils pour soutenir efficacement une personne danssa recherche professionnelle et dans sa prise de poste</p>
+              </div>
+              <div className="uk-width-1-3">
+                <img src="/static/img/help_3.png" width="100%"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="uk-section uk-section-default">
+      <div className="uk-container">
+        <div className="uk-text-center" id="scale">
+          <h1>Aider <span className="uk-text-primary">à votre échelle</span></h1>
+          <div className="uk-child-width-1-2" data-uk-grid>
+            <div>
+              <div className="uk-card uk-card-body">
+                <h2>Je suis <span className="uk-text-primary">un particulier</span></h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam repellat suscipit, quo iure similique beatae recusandae eius itaque.</p>
+              </div>
+            </div>
+            <div>
+              <div className="uk-card uk-card-body">
+                <h2>Je suis <span className="uk-text-primary">un particulier</span></h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam repellat suscipit, quo iure similique beatae recusandae eius itaque.</p>
+              </div>
+            </div>
+            <div>
+              <div className="uk-card uk-card-body">
+                <h2>Je suis <span className="uk-text-primary">un particulier</span></h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam repellat suscipit, quo iure similique beatae recusandae eius itaque.</p>
+              </div>
+            </div>
+            <div>
+              <div className="uk-card uk-card-body">
+                <h2>Je suis <span className="uk-text-primary">un particulier</span></h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam repellat suscipit, quo iure similique beatae recusandae eius itaque.</p>
+              </div>
+            </div>
+          </div>
+          <button className="uk-button uk-button-default uk-text-uppercase">écrivez-nous</button>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default JeVeuxAider;
