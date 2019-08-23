bc2368a28026e538d9a402cfd5da7e7d36f43e22
Clement <clement.le.bars@accenture.com>
2019-08-23 17:23:09 +0200

    Add component ReviewCard

--- pages/JeVeuxRecruter.js
+++ pages/JeVeuxRecruter.js
@@ -1,98 +1,76 @@
 import React, { Fragment } from 'react';
-import { HelpingCard, ScaleCard, Button, Section } from '../components/utils';
+import { HelpingCard, ReviewCard, Button, Section } from '../components/utils';
 import { GridNoSSR } from '../components/utils/Grid';
 import { IconNoSSR } from '../components/utils/Icon';
 
-const JeVeuxRecruter = () => (
-  <Fragment>
-    <Section id="recruter1" style="default">
-      <h1 className="uk-text-bold uk-text-center">
-        Vous souhaitez <span className="uk-text-primary">recruter</span> un candidat ?
-      </h1>
-      <div className="uk-width-1-2 uk-align-center uk-text-center">
-        <p>
-          Recruteurs, plus que quiconque, faîtes la différence !
-          Soyez acteur essentiel de l'opération LinkedOut par Entourage
-          en publiant vos offres d'emplois et en cherchant les candidats
-          qui feront le bonheur de votre entreprise.
-        </p>
-        <Button style="primary">Découvrir les candidats</Button>
-      </div>
-    </Section>
-    <Section id="recruter2" style="default">
-      <h2 className="uk-text-bold uk-align-center uk-text-center uk-width-1-2 uk-margin-large-bottom">
-        Une opportunité <span className="uk-text-primary">et</span> une bonne action,
-        <span className="uk-text-primary"> ne passez pas à côté !</span>
-      </h2>
-      <div data-uk-grid className="uk-child-width-expand@s">
-        <div>
-          <div className="uk-width-5-6">
-            <h3 className="uk-text-bold"><span className="uk-text-primary">LinedOut plebiscité</span> par les recruteurs...</h3>
-            <p>
-              Le retour des recruteurs en entreprise est unanime : les candidats apportent une joie de
-              vivre et une énergie qui facilitent leur intégration. Un bonheur au quotidien.
-            </p>
+
+
+const JeVeuxRecruter = () => {
+  const reviews = [{
+    author: "Paul Jean",
+    colorClass: "uk-text-primary",
+    picture: "/static/img/arthur.png",
+    review: "Nous avons intégré Zineb à l'équipe il y a maintenant 6 mois. Tout a été rendu facile par Entourage et les équipes de travail sont très satisfaites.",
+    role: "Directeur RH chez Sanofi"
+  }, {
+    author: "Stéphane Joli",
+    colorClass: "",
+    picture: "/static/img/arthur.png",
+    review: "Mohamed s'est fondu dans le groupe en un clin d'oeil, tout le monde a l'impression qu'il est là depuis 10 ans.",
+    role: "Recruteur pour LVMH"
+  }, {
+    author: "Jeanne Pierrot ",
+    colorClass: "uk-text-primary",
+    picture: "/static/img/arthur.png",
+    review: "La réinsertion est rendue plus facile grâce à Entourage Jobs, et c'est une belle victoire pour les candidats et pour les entreprises. Continuez !",
+    role: "Directrice RH chez Le grand Breguet"
+  }];
+
+  return (
+    <Fragment>
+      <Section id="recruter1" style="default">
+        <h1 className="uk-text-bold uk-text-center">
+          Vous souhaitez <span className="uk-text-primary">recruter</span> un candidat ?
+        </h1>
+        <div className="uk-width-1-2 uk-align-center uk-text-center">
+          <p>
+            Recruteurs, plus que quiconque, faîtes la différence !
+            Soyez acteur essentiel de l'opération LinkedOut par Entourage
+            en publiant vos offres d'emplois et en cherchant les candidats
+            qui feront le bonheur de votre entreprise.
+          </p>
+          <Button style="primary">Découvrir les candidats</Button>
+        </div>
+      </Section>
+      <Section id="recruter2" style="default">
+        <h2 className="uk-text-bold uk-align-center uk-text-center uk-width-1-2 uk-margin-large-bottom">
+          Une opportunité <span className="uk-text-primary">et</span> une bonne action,
+          <span className="uk-text-primary"> ne passez pas à côté !</span>
+        </h2>
+        <div data-uk-grid className="uk-child-width-expand@s">
+          <div>
+            <div className="uk-width-5-6">
+              <h3 className="uk-text-bold"><span className="uk-text-primary">LinkedOut plebiscité</span> par les recruteurs...</h3>
+              <p>
+                Le retour des recruteurs en entreprise est unanime : les candidats apportent une joie de
+                vivre et une énergie qui facilitent leur intégration. Un bonheur au quotidien.
+              </p>
+            </div>
+          </div>
+          <div>
+            {reviews.map((review) => (
+              <ReviewCard
+                author={review.author}
+                colorClass={review.colorClass}
+                picture={review.picture}
+                review={review.review}
+                role={review.role} />
+            ))}
           </div>
         </div>
-        <div>
-          <div class="uk-card uk-card-default uk-card-body uk-margin-medium">
-            <div data-uk-grid>
-              <div className="uk-width-auto uk-text-primary">
-                <IconNoSSR name="quote-right" />
-              </div>
-              <div className="uk-width-expand">
-                <p className="uk-text-small uk-margin-small">
-                  Nous avons intégré Zineb à l'équipe il y a maintenant 6 mois.
-                  Tout a été rendu facile par Entourage et les équipes de travail sont très satisfaites.
-                </p>
-                <p className="uk-text-bold uk-margin-small uk-margin-remove-bottom">Paul Jean</p>
-                <p className="uk-margin-remove">Directeur RH chez Sanofi</p>
-              </div>
-              <div className="uk-width-auto uk-text-bottom" style={{ alignSelf: "flex-end" }}>
-                <img src="/static/img/arthur.png" alt="Arthur" style={{ width: "80px", height: "80px", borderRadius: "50%" }} />
-              </div>
-            </div>
-          </div>
-          <div class="uk-card uk-card-default uk-card-body uk-margin-medium">
-            <div data-uk-grid>
-              <div className="uk-width-auto uk-text-primary">
-                <IconNoSSR name="quote-right" />
-              </div>
-              <div className="uk-width-expand">
-                <p className="uk-text-small uk-margin-small">
-                  Nous avons intégré Zineb à l'équipe il y a maintenant 6 mois.
-                  Tout a été rendu facile par Entourage et les équipes de travail sont très satisfaites.
-                </p>
-                <p className="uk-text-bold uk-margin-small uk-margin-remove-bottom">Paul Jean</p>
-                <p className="uk-margin-remove">Directeur RH chez Sanofi</p>
-              </div>
-              <div className="uk-width-auto uk-text-bottom" style={{ alignSelf: "flex-end" }}>
-                <img src="/static/img/arthur.png" alt="Arthur" style={{ width: "80px", height: "80px", borderRadius: "50%" }} />
-              </div>
-            </div>
-          </div>
-          <div class="uk-card uk-card-default uk-card-body uk-margin-medium">
-            <div data-uk-grid>
-              <div className="uk-width-auto uk-text-primary">
-                <IconNoSSR name="quote-right" />
-              </div>
-              <div className="uk-width-expand">
-                <p className="uk-text-small uk-margin-small">
-                  Nous avons intégré Zineb à l'équipe il y a maintenant 6 mois.
-                  Tout a été rendu facile par Entourage et les équipes de travail sont très satisfaites.
-                </p>
-                <p className="uk-text-bold uk-margin-small uk-margin-remove-bottom">Paul Jean</p>
-                <p className="uk-margin-remove">Directeur RH chez Sanofi</p>
-              </div>
-              <div className="uk-width-auto uk-text-bottom" style={{ alignSelf: "flex-end" }}>
-                <img src="/static/img/arthur.png" alt="Arthur" style={{ width: "80px", height: "80px", borderRadius: "50%" }} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Section>
-  </Fragment>
-);
+      </Section>
+    </Fragment>
+  );
+};
 
 export default JeVeuxRecruter;
