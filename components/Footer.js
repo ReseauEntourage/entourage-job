348170059644e2ba2cce5357c687e6c087bf355d
Hospice <johannhospice.dev@gmail.com>
2019-08-27 17:58:33 +0200

    Update footer, header and app
    Rename app, change links locations and remove useless comments

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { GridNoSSR, IconNoSSR, Section, SimpleLink } from './utils';
-/**
- *
- * links: Array<{href: str, name: str}>
- */
-const Footer = ({ items }) => {
+
+const Footer = () => {
   return (
     <footer id="footer">
-      {/* className="uk-section uk-section-secondary" */}
       <Section style="secondary" size="large">
         <GridNoSSR
           childWidths={['1-3']}
@@ -62,15 +57,6 @@ const Footer = ({ items }) => {
       </Section>
     </footer>
   );
-};
-Footer.propTypes = {
-  items: PropTypes.arrayOf(
-    PropTypes.shape({
-      href: PropTypes.string,
-      key: PropTypes.string,
-      name: PropTypes.string,
-    })
-  ).isRequired,
 };
 
 export default Footer;
