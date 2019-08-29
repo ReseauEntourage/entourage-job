2873c1135ca1403057be9e7e3efbfb6bbf20de35
Clement <clement.le.bars@accenture.com>
2019-08-29 13:44:41 +0200

    Improve responsive page

--- pages/JeVeuxTravailler.js
+++ pages/JeVeuxTravailler.js
@@ -30,25 +30,30 @@ const JeVeuxTravailler = () => {
         <HowTo ccm={ccm} />
       </Section>
       <Section id="travailler3" style="secondary" size="small">
-        <p className="uk-text-lead uk-width-2-3 uk-text-center uk-align-center">
+        <p className="uk-text-lead uk-text-center uk-align-center uk-width-2-3@s">
           Nos candidats ont des papiers d'identités, une autorisation de travail sur le territoire français, un compte bancaire et une domiciliation.<br />
           <span className="uk-text-bold">Nous vous aidons à réunir les mêmes conditions.</span>
         </p>
       </Section>
       <Section id="travailler4">
-        <h2 className="uk-text-bold uk-text-center uk-align-center uk-width-3-5">
+        <h2 className="uk-text-bold uk-text-center uk-align-center uk-width-3-5@s">
           Là pour vous,<br />
           <span className="uk-text-primary">qu'elle que soit votre situation</span>
         </h2>
-        <div className="uk-margin-large-top uk-child-width-1-2@s uk-grid-match" data-uk-grid>
+        <div className="uk-margin-large-top uk-child-width-1-2@m uk-child-width-1-1 uk-grid-match uk-grid-small" data-uk-grid>
           <div>
             <div className="uk-card uk-card-large uk-card-default uk-card-body">
-              <div data-uk-grid>
-                <div className="uk-width-1-1@s uk-width-small@m uk-flex uk-flex-center ">
-                  <img src="/static/img/illustrations/friendship.png" alt="1" className="uk-height-max-small" />
+              <div className="uk-panel uk-flex uk-flex-middle" data-uk-grid>
+                <div className="uk-width-auto@s uk-width-1-1 uk-text-center">
+                  <img
+                    src="/static/img/illustrations/friendship.png"
+                    style={{ width: "150px", height: "150px" }}
+                    alt="1"
+                    className="uk-margin-remove-bottom uk-margin-small-right"
+                  />
                 </div>
-                <div className="uk-width-expand">
-                  <p className="uk-text-center uk-text-left@m">
+                <div className="uk-width-expand@s uk-width-1-1 uk-text-left@m uk-text-center">
+                  <p>
                     Je suis prêt à intégrer le dispositif
                   </p>
                   <Button style="primary">Je postule</Button>
@@ -58,15 +63,20 @@ const JeVeuxTravailler = () => {
           </div>
           <div>
             <div className="uk-card uk-card-large uk-card-default uk-card-body">
-              <div data-uk-grid>
-                <div className="uk-width-expand">
-                  <p className="uk-text-center uk-text-left@m">
+              <div className="uk-panel uk-flex uk-flex-middle" data-uk-grid>
+                <div className="uk-width-auto@s uk-width-1-1 uk-text-center">
+                  <img
+                    src="/static/img/illustrations/entourage_help.png"
+                    style={{ width: "150px", height: "150px" }}
+                    alt="2"
+                    className="uk-margin-remove-bottom uk-margin-small-left"
+                  />
+                </div>
+                <div className="uk-width-expand uk-text-left@m uk-text-center uk-flex-first@s">
+                  <p >
                     Je me fais aider dans les démarches
                   </p>
                   <Button style="default">J'ai besoin d'aide</Button>
-                </div>
-                <div className="uk-width-1-1@s uk-width-small@m uk-flex uk-flex-center ">
-                  <img src="/static/img/illustrations/entourage_help.png" alt="2" className="uk-height-max-small" />
                 </div>
               </div>
             </div>
@@ -74,18 +84,18 @@ const JeVeuxTravailler = () => {
         </div>
       </Section>
       <Section id="travailler5" style="default">
-        <h4 className="uk-text-bold uk-text-center uk-align-center uk-width-3-5">
+        <h4 className="uk-text-bold uk-text-center uk-align-center uk-width-3-5@s">
           Vous souhaitez <span className="uk-text-primary">nous contacter et discuter ?</span>
         </h4>
         <div
-          className="uk-align-center uk-text-center uk-margin-small-top"
+          className="uk-align-center uk-text-center uk-margin-large-top"
           data-uk-grid
         >
-          <div className="uk-inline uk-padding-remove">
+          <div className="uk-inline uk-padding-small">
             <Button style="primary">écrivez-nous</Button>
           </div>
-          <div className="uk-inline">
-            <Button>Rappelez-moi</Button>
+          <div className="uk-inline uk-padding-small uk-margin-remove">
+            <Button style="default">Rappelez-moi</Button>
           </div>
         </div>
       </Section>
