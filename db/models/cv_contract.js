84bde3b4f175c23882673cc4c27f1af28935a2f4
Clement <clement.le.bars@accenture.com>
2019-10-02 17:38:55 +0200

    S50 :  Create "Contract" and "CV_Contract" models and migration files

---
+++ db/models/cv_contract.js
@@ -0,0 +1,30 @@
+/* eslint-disable camelcase */
+
+module.exports = (sequelize, DataTypes) => {
+  const CV_Contract = sequelize.define(
+    'CV_Contract',
+    {
+      CVId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+          model: 'CVs',
+          key: 'id',
+        },
+      },
+      contractId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+          model: 'Contracts',
+          key: 'id',
+        },
+      },
+    },
+    {}
+  );
+  CV_Contract.associate = function(models) {
+    // associations can be defined here
+  };
+  return CV_Contract;
+};
