e54c83e10c68e3b9a78a48eb5f9ab2568f1503f8
Clement <clement.le.bars@accenture.com>
2019-10-11 16:57:11 +0200

    S131 : DiscoverPartial is now random !

--- components/partials/DiscoverPartial.js
+++ components/partials/DiscoverPartial.js
@@ -1,34 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { GridNoSSR, Section, IconNoSSR } from '../utils';
 import { CandidatCard } from '../cards';
+import Api from '../../Axios';
 
-const DiscoverPartial = () => (
-  <Section id="discover">
-    <div className="uk-text-center">
-      <h2 className="uk-text-bold">
-        Découvrez les <span className="uk-text-primary">candidats</span>
-      </h2>
-      <a href="#">
-        Voir tous les candidats <IconNoSSR name="arrow-right" />
-      </a>
-    </div>
-    <div className="uk-margin-large">
-      <GridNoSSR
-        childWidths={['1-1', '1-2@s']}
-        items={Array(2).fill(
-          <CandidatCard
-            imgSrc="/static/img/arthur.png"
-            imgAlt="arthur"
-            title="Arthur"
-            id="arthur"
-            description="série télévisée d'animation américano-canadienne."
-            goods={['volontaire', "esprit d'équipe"]}
-            ambitions={['la vente', 'la restauration']}
+export default class DiscoverPartial extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listCVs: [],
+    };
+  }
+
+  componentDidMount() {
+    Api.get('/api/v1/cv/cards/random?nb=2')
+      .then((res) => {
+        if (res.data) {
+          /** Liste de CVs limitée à 2 profils */
+          const CVs = res.data.slice(0, 2);
+          this.setState({ listCVs: CVs });
+        } else {
+          this.setState({ listCVs: [] });
+        }
+      })
+      .catch(() => {
+        return console.log('Impossible de récupérer les CVs.');
+      });
+  }
+
+  render() {
+    const { listCVs } = this.state;
+    if (listCVs.length === 0) {
+      return null;
+    }
+    return (
+      <Section id="discover">
+        <div className="uk-text-center">
+          <h2 className="uk-text-bold">
+            Découvrez les <span className="uk-text-primary">candidats</span>
+          </h2>
+          <a href="#">
+            Voir tous les candidats <IconNoSSR name="arrow-right" />
+          </a>
+        </div>
+        <div className="uk-margin-large">
+          <GridNoSSR
+            childWidths={['1-1', '1-2@s']}
+            items={listCVs.map((cv) => {
+              return (
+                <CandidatCard
+                  url={cv.url}
+                  imgSrc="static/img/arthur.png"
+                  imgAlt={cv.firstName}
+                  firstName={cv.firstName}
+                  ambitions={cv.Skills.slice(0, 2)}
+                />
+              );
+            })}
           />
-        )}
-      />
-    </div>
-  </Section>
-);
-
-export default DiscoverPartial;
+        </div>
+      </Section>
+    );
+  }
+}
