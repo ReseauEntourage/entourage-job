612010c667e1a3ebc99f9f1fcddc1a8ad67b4c6e
Clement <clement.le.bars@accenture.com>
2019-10-15 11:21:05 +0200

    Fix Axios BaseURL (to test in preprod)

--- Axios.js
+++ Axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-  baseURL: `${process.env.SERVER_URL}:${process.env.PORT}`,
+  baseURL: `${process.env.SERVER_URL}${process.env.PORT &&
+    `:${process.env.PORT}`}`,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
