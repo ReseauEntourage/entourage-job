a1c63a23ab4b60fa45455e046f5eea6ec93642f4
Clement <clement.le.bars@accenture.com>
2019-11-07 17:45:01 +0100

    S144 : Add model User

---
+++ backend/db/models/user.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    email: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    role: DataTypes.STRING,
+    isAdmin: DataTypes.BOOLEAN,
+    password: DataTypes.STRING
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
