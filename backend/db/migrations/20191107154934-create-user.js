c531e26dd227410917a2975ad234444148a98357
Clement <clement.le.bars@accenture.com>
2019-11-08 14:12:42 +0100

    S144 : Create User model and table

--- backend/db/migrations/20191107154934-create-user.js
+++ backend/db/migrations/20191107154934-create-user.js
@@ -1,42 +1,49 @@
-'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('Users', {
       id: {
         allowNull: false,
-        autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.UUID,
       },
       email: {
-        type: Sequelize.STRING
+        allowNull: false,
+        type: Sequelize.STRING,
       },
       firstName: {
-        type: Sequelize.STRING
+        allowNull: false,
+        type: Sequelize.STRING,
       },
       lastName: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       role: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       isAdmin: {
-        type: Sequelize.BOOLEAN
+        allowNull: false,
+        defaultValue: false,
+        type: Sequelize.BOOLEAN,
       },
       password: {
-        type: Sequelize.STRING
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      salt: {
+        allowNull: false,
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
-  down: (queryInterface, Sequelize) => {
+  down: (queryInterface) => {
     return queryInterface.dropTable('Users');
-  }
+  },
 };
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,13 +1,67 @@
-'use strict';
+const uuid = require('uuid/v4');
+
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    email: DataTypes.STRING,
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    role: DataTypes.STRING,
-    isAdmin: DataTypes.BOOLEAN,
-    password: DataTypes.STRING
-  }, {});
+  const User = sequelize.define(
+    'User',
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: 'Une adresse email doit être renseignée',
+          },
+        },
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [0, 40],
+            msg: '40 caractères maximum pour le prénom',
+          },
+          notEmpty: {
+            args: true,
+            msg: 'Un prénom est requis',
+          },
+        },
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [0, 40],
+            msg: '40 caractères maximum pour le prénom',
+          },
+          notEmpty: {
+            args: true,
+            msg: 'Un prénom est requis',
+          },
+        },
+      },
+      role: DataTypes.STRING,
+      isAdmin: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      salt: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {}
+  );
+  User.beforeCreate((user) => {
+    return (user.id = uuid());
+  });
   User.associate = function(models) {
     // associations can be defined here
   };
