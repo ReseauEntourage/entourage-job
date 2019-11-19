b97d3a3c355d8ccd9eb4ce2afb5550b4a90537ae
Hospice <johannhospice.dev@gmail.com>
2019-11-19 17:19:04 +0100

    Add automated shared image test

---
+++ test/shareImage.test.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const generateCVPreview = require('../backend/shareImage');
+
+describe('Shared image', () => {
+  const output = './mytest.jpg';
+  const input = '../static/img/arthur.png';
+
+  after(() => {
+    fs.unlinkSync(output);
+  });
+
+  it('should generate an image', () => {
+    const ambitions = ['amb1', 'amb2'];
+    const catchphrase = "A besoin d'un coup de pouce pour travailler dans...";
+    const title = 'MON TEST';
+    return generateCVPreview(
+      title,
+      catchphrase,
+      ambitions.join('. ').toUpperCase(),
+      input,
+      output
+    );
+  });
+});
