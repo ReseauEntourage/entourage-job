e63b69f6ff8915e3a84d4212708784cf0cecc68e
Hospice <johannhospice.dev@gmail.com>
2020-01-06 18:09:25 +0100

    update footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,50 @@
 import React from 'react';
-import { GridNoSSR, IconNoSSR, Section, SimpleLink, ImgNoSSR } from './utils';
+import {
+  GridNoSSR,
+  IconNoSSR,
+  Section,
+  SimpleLink,
+  ImgNoSSR,
+  Background,
+} from './utils';
 
 const Footer = () => {
   return (
     <footer id="footer">
+      <Background blend={{ colorHex: '#484848' }}>
+        <Section size="small">
+          <GridNoSSR
+            center
+            column
+            childWidths={['auto']}
+            className="uk-text-center"
+          >
+            <SimpleLink href="https://www.entourage.social" isExternal>
+              <ImgNoSSR
+                src="/static/img/logo-entourage.svg"
+                alt="logo-entourage"
+              />
+            </SimpleLink>
+            <h3 className="uk-text-bold">
+              <span style={{ color: '#fff' }}>L&apos;association </span>
+              <span className="uk-text-primary">Entourage</span>
+            </h3>
+            <GridNoSSR eachWidths={['expand', 'auto', 'expand']}>
+              <div style={{ color: '#fff' }}>
+                <p className="uk-text-left">
+                  Entourage est une association fondée en 2014 qui vise à créer
+                  ?du lien social entre riverains et personnes SDF notamment
+                  grâce à l’application mobile 'Entourage' qui permet de
+                  coordonner des actions de solidarité à l’échelle locale.
+                </p>
+                <button>Voir le site ></button>
+              </div>
+              <hr className="uk-divider-vertical uk-divider-small" />
+              <div />
+            </GridNoSSR>
+          </GridNoSSR>
+        </Section>
+      </Background>
       <Section style="secondary" size="medium">
         <GridNoSSR
           childWidths={['1-5@s', '1-2']}
@@ -30,13 +71,6 @@ const Footer = () => {
           ]}
         />
         <div className="uk-flex-center uk-flex uk-flex-middle uk-margin-top">
-          <SimpleLink href="https://www.entourage.social" isExternal>
-            <ImgNoSSR
-              src="/static/img/logo-entourage.svg"
-              alt="logo-entourage"
-              className="uk-padding"
-            />
-          </SimpleLink>
           <hr className="uk-divider-vertical uk-margin-small-right uk-margin-small-left" />
           <div className="uk-flex-center uk-flex uk-flex-middle">
             <SimpleLink
