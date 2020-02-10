b83b40a83079708fd521b43649139115f87d0a07
Hospice <johannhospice.dev@gmail.com>
2020-02-10 18:57:28 +0100

    Update edit picture view

--- components/cv/CVEditPicture.js
+++ components/cv/CVEditPicture.js
@@ -5,52 +5,47 @@ import axios from '../../Axios';
 
 const CVEditPicture = ({ urlImg, onChange }) => {
   return (
-    <>
-      <div
-        className="uk-card uk-height-1-1 uk-background-cover uk-background-blend-multiply uk-background-secondary"
-        style={{ backgroundImage: `url(${urlImg})`, minHeight: '300px' }}
-      />
-      <div className="js-upload" data-uk-form-custom>
-        <input
-          type="file"
-          onChange={async ({ target }) => {
-            const file = target.files[0];
-            try {
-              const { data } = await axios.post('/api/v1/cv/image', { file });
-              // todo
-
-              onChange({ urlImg: data.urlImg });
-              UIkit.notification('Photo enregistrée', 'success');
-            } catch (err) {
-              UIkit.notification(
-                "Erreur lors de l'enregistrement de la photo",
-                'danger'
-              );
-            }
-          }}
-        />
-        <button
-          className="uk-button uk-button-default"
-          type="button"
-          tabIndex="-1"
-          on
-        >
-          Select
-        </button>
+    <div
+      className="uk-card uk-height-1-1 uk-background-cover "
+      style={{ backgroundImage: `url(${urlImg})`, minHeight: '300px' }}
+    >
+      <div className="uk-overlay-default uk-position-cover" data-uk-form-custom>
+        <div className="uk-position-center">
+          <input
+            type="file"
+            onChange={({ target }) => {
+              const file = target.files[0];
+              axios
+                .post('/api/v1/cv/image', { file })
+                .then(({ data }) => {
+                  onChange({ urlImg: data.urlImg });
+                  UIkit.notification('Photo enregistrée', 'success');
+                })
+                .catch((err) => {
+                  console.error(err);
+                  UIkit.notification(
+                    "Erreur lors de l'enregistrement de la photo",
+                    'danger'
+                  );
+                });
+            }}
+          />
+          <button
+            className="uk-button uk-button-default"
+            type="button"
+            tabIndex="-1"
+            on
+          >
+            Select
+          </button>
+        </div>
       </div>
-      {/* <span uk-icon="plus" /> */}
-      {/* <span>Mettre à jour</span>
-      <ModalGeneric id="modal-update-image">
-        {(close) => (
-        )}
-      </ModalGeneric>
-    </div> */}
-    </>
+    </div>
   );
 };
 CVEditPicture.propTypes = {
   urlImg: PropTypes.string,
-  onChange: PropTypes.func,
+  onChange: PropTypes.func.isRequired,
 };
 CVEditPicture.defaultProps = {
   urlImg: '../../static/img/arthur-background.jpg',
