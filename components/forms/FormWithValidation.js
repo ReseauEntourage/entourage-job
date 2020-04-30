aedfc273661bc5e2874f7398c45dc19a8d8fb454
Hospice <johannhospice.dev@gmail.com>
2020-04-30 17:35:44 +0200

    Fix error messages on form

--- components/forms/FormWithValidation.js
+++ components/forms/FormWithValidation.js
@@ -6,7 +6,7 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import FooterForm from '../utils/FooterForm';
 import FormValidator from './FormValidator';
-import FieldFactory from './fields/FieldFactory'; // eslint-disable-line import/no-cycle
+import generate from './fieldGenerator';
 
 const FormWithValidationV2 = ({
   formSchema: { id, rules, fields },
@@ -15,102 +15,79 @@ const FormWithValidationV2 = ({
   onSubmit,
   onCancel,
 }) => {
-  const [validator, setValidator] = useState();
+  const validator = new FormValidator(rules);
 
   const [usedDefaultValues, setUsedDefaultValues] = useState(defaultValues);
   const [error, setError] = useState();
-  const [fieldFactory, setFieldFactory] = useState();
 
   const [fieldValidations, setFieldValidations] = useState({});
   const [fieldValues, setFieldValues] = useState({});
-
-  const setValid = (key, value) =>
-    setFieldValidations({ ...fieldValidations, [`valid_${key}`]: value });
-  const setValue = (key, value) =>
-    setFieldValues({ ...fieldValues, [key]: value });
-
-  const getValid = (key) => fieldValidations[`valid_${key}`];
-  const getValue = (key) => fieldValues[key];
 
   // fonction permettant de verifier une champs d'entré utilisateur
   const updateForm = ({
     target: { checked, name, type, value, selectedIndex },
   }) => {
-    if (validator) {
-      let fieldValue;
-      if (type === 'checkbox') {
-        fieldValue = checked;
-      } else if (type === 'select-one' && selectedIndex === 0) {
-        fieldValue = null; // si on est sur le placeholder ( option sans valeur )
-      } else fieldValue = value;
+    let fieldValue;
+    if (type === 'checkbox') {
+      fieldValue = checked;
+    } else if (type === 'select-one' && selectedIndex === 0) {
+      fieldValue = null; // si on est sur le placeholder ( option sans valeur )
+    } else fieldValue = value;
 
-      /* Validators start */
-      setValue(name, fieldValue); // enregistre la valeur du champs
-      const validation = validator.validate(fieldValues); // envoie une copie des champs pour que le state ne soit pas altéré
+    /* Validators start */
+    const tmpFieldValues = fieldValues;
+    tmpFieldValues[name] = fieldValue;
+    setFieldValues(tmpFieldValues); // enregistre la valeur du champs
+    const validation = validator.validate(tmpFieldValues); // envoie une copie des champs pour que le state ne soit pas altéré
 
-      // enregistre la raison de la validation {isInvalid: boolean, message: string}
-      if (validation[name] !== undefined) {
-        setValid(name, validation[name]);
-      }
-
-      /* Validators end */
-      setError('');
+    // enregistre la raison de la validation {isInvalid: boolean, message: string}
+    if (validation[name] !== undefined) {
+      const tmpFieldValidations = fieldValidations;
+      tmpFieldValidations[`valid_${name}`] = validation[name];
+      setFieldValidations(tmpFieldValidations);
     }
+
+    /* Validators end */
+    setError('');
   };
 
   const submitForm = (event) => {
-    if (validator) {
-      event.preventDefault();
-      // Vérifie les champs avant soumission
-      /* Validators control before submit */
-      const validation = validator.validate(fieldValues);
-      if (validation.isValid) {
-        // Si les validators sont OK.
-        onSubmit(fieldValues, (msg) => setError(msg)); // c'est le props onsubmit de FormWithValidation
-      } else {
-        // erreur de validation
-        Object.keys(validation).forEach((key) => {
-          if (key !== 'isValid') {
-            setValid(key, validation[key]);
-          }
-        });
-        setError('Un ou plusieurs champs sont invalides');
-        console.error({ validation });
-      }
+    event.preventDefault();
+    // Vérifie les champs avant soumission
+    /* Validators control before submit */
+    const validation = validator.validate(fieldValues);
+    if (validation.isValid) {
+      // Si les validators sont OK.
+      onSubmit(fieldValues, (msg) => setError(msg)); // c'est le props onsubmit de FormWithValidation
+    } else {
+      // erreur de validation
+      const tmpFieldValidations = fieldValidations;
+      Object.keys(validation).forEach((key) => {
+        if (key !== 'isValid') {
+          tmpFieldValidations[`valid_${key}`] = validation[key];
+        }
+      });
+      setFieldValidations(tmpFieldValidations);
+      setError('Un ou plusieurs champs sont invalides');
+      console.error(validation);
     }
   };
 
   useEffect(() => {
-    setValidator(new FormValidator(rules));
-
-    // todo revoir la structure du state
     // on extrait les nom des champs
     const fieldsId = fields.map((field) => field.id);
 
-    setFieldValues(
-      fieldsId.reduce((acc, value) => {
-        acc[value] = usedDefaultValues[value];
-        return acc;
-      }, {})
-    );
+    const validations = fieldsId.reduce((acc, value) => {
+      acc[`valid_${value}`] = undefined;
+      return acc;
+    }, {});
+    const values = fieldsId.reduce((acc, value) => {
+      acc[value] = usedDefaultValues[value];
+      return acc;
+    }, {});
 
-    setFieldValidations(
-      fieldsId.reduce((acc, value) => {
-        acc[`valid_${value}`] = undefined;
-        return acc;
-      }, {})
-    );
-
-    setFieldFactory(
-      new FieldFactory(
-        id,
-        fields,
-        usedDefaultValues,
-        updateForm,
-        getValid,
-        getValue
-      )
-    );
+    setFieldValues(values);
+    setFieldValidations(validations);
   }, [fields, usedDefaultValues]);
 
   return (
@@ -122,10 +99,18 @@ const FormWithValidationV2 = ({
         onSubmit={submitForm}
       >
         <fieldset className="uk-fieldset uk-width-1-1">
-          {fieldFactory &&
-            fields.map((value, i) => (
-              <li key={i}>{fieldFactory.generate(value)}</li>
-            ))}
+          {fields.map((value, i) => (
+            <li key={i}>
+              {generate(
+                value,
+                id,
+                usedDefaultValues,
+                updateForm,
+                (name) => fieldValidations[`valid_${name}`],
+                (name) => fieldValues[name]
+              )}
+            </li>
+          ))}
         </fieldset>
         <div>
           <FooterForm
