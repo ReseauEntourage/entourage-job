cf105ed021688a7dc1f71aa6f4cfaa881ae78f98
Emile Bex <emile.bex@gmail.com>
2020-04-30 18:37:55 +0200

    [Landing Page] Update address receiving emails Added usage of Airtable to fill table everytime  somebody fills the form, as well as sending the email out

--- backend/lib/aws_lambda/handler.js
+++ backend/lib/aws_lambda/handler.js
@@ -1,5 +1,5 @@
-'use strict';
 const Mailjet = require('node-mailjet');
+const Airtable = require('airtable');
 
 const mailjet = Mailjet.connect(
   process.env.MAILJET_PUB,
@@ -8,28 +8,31 @@ const mailjet = Mailjet.connect(
 
 const send = mailjet.post('send');
 
+const base = new Airtable({apiKey: process.env.AIRTABLE_KEY}).base(process.env.AIRTABLE_BASE);
+
 const sendMail = ({subject, text, html}) => new Promise((res, rej) => {
   send.request({
     FromEmail: process.env.MAILJET_FROM_EMAIL,
     FromName: process.env.MAILJET_FROM_NAME,
-    Recipients: [{ Email: process.env.MAILJET_TO_EMAIL }],
+    Recipients: [{Email: process.env.MAILJET_TO_EMAIL}],
     Subject: subject,
     'Text-part': text,
     'HTML-part': html,
   }).then((mailjetRes) => {
-    //console.log(mailjetRes);
+    // console.log(mailjetRes);
     res();
   }).catch((mailjetErr) => {
-    //console.log(mailjetErr);
+    // console.log(mailjetErr);
     rej();
-  }) ;
+  });
 });
 
 module.exports.sendContactInfoByMail = (event, context, callback) => {
-  let res = {};
+  const res = {};
 
-  let contactDetails = (typeof event.body === "object") ? event.body : JSON.parse(event.body)
-  if(contactDetails.security === "") {
+  const contactDetails = (typeof event.body === "object") ? event.body : JSON.parse(event.body);
+
+  if (contactDetails.security === "") {
     delete contactDetails.security;
     const emailObject = {
       subject: "Vendée Globe - Formulaire de contact ⛵️",
@@ -42,22 +45,41 @@ module.exports.sendContactInfoByMail = (event, context, callback) => {
         `Message : ${contactDetails.message || ''}\n`
     };
     sendMail(emailObject).then(() => {
-      res.statusCode = 200
-      res.headers= {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-      }
-      res.body = JSON.stringify({message: "Mail sent !"});
-      callback(null, res);
-    }).catch((err) => {
-      res.statusCode = 500
-      res.body = JSON.stringify({message: "Mail failed to send."});
+      base('Réponses').create([
+        {
+          "fields": {
+            "Nom & Prénom": contactDetails.name || '',
+            "Status": contactDetails.status || '',
+            "Organisation": contactDetails.organisation || '',
+            "Email": contactDetails.mail || '',
+            "Numéro de téléphone": contactDetails.telephone || '',
+            "Message": contactDetails.message || ''
+          }
+        }
+      ], (err, records) => {
+        if (err) {
+          res.statusCode = 500;
+          res.body = JSON.stringify({message: "Mail sent but table failed to get updated."});
+          callback(null, res);
+        } else {
+          res.statusCode = 200;
+          res.headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+          };
+          res.body = JSON.stringify({message: "Mail sent and table updated !"});
+        }
+
+      });
+    }).catch(() => {
+      res.statusCode = 500;
+      res.body = JSON.stringify({message: "Mail failed to send and table failed to get updated."});
       callback(null, res);
     })
-  }
-  else {
+  } else {
     res.statusCode = 400;
-    res.body = JSON.stringify({message: "Form values not correct.",
+    res.body = JSON.stringify({
+      message: "Form values not correct.",
       debug_forms: contactDetails
     });
     callback(null, res);
