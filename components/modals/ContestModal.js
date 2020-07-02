200741dd86634d13a7e6ce56df983e63565bacdd
Emile Bex <emile.bex@gmail.com>
2020-07-02 11:06:34 +0200

    Fix bug where the modal was unmounted  when it was already unmounted because it didn't have any parent

--- components/modals/ContestModal.js
+++ components/modals/ContestModal.js
@@ -1,3 +1,5 @@
+/* global UIkit */
+
 import React, {useState, useEffect} from 'react';
 import {Button, CloseButtonNoSSR, IconNoSSR} from "../utils";
 import {EXTERNAL_LINKS} from '../../constants';
@@ -10,7 +12,7 @@ const ContestModal = () => {
     const countDownDate = new Date(2020, 6, 4, 15, 30, 0).getTime() ;
 
     // Update the count down every 1 second
-    const x = setInterval(function() {
+    const updateInterval = setInterval(() => {
 
       // Get today's date and time
       const now = new Date().getTime();
@@ -30,38 +32,47 @@ const ContestModal = () => {
 
       // If the count down is finished, write some text
       if (distance < 0) {
-        clearInterval(x);
+        clearInterval(updateInterval);
         setCountdown("C'est parti\xa0!");
       }
     }, 1000);
+
+    UIkit.modal(`#modal-contest`).show();
+
+    return () => {
+      clearInterval(updateInterval);
+    }
+
   }, []);
 
   return(
-    <div id="modal-contest" uk-modal>
-      <div className="uk-modal-dialog uk-modal-body uk-background-primary">
-        <button
-          className="uk-modal-close-default"
-          type="button"
-          data-uk-close
-          style={{color: 'white'}}
-          aria-label="close"
-        />
-        <div className="uk-inline">
-          <img src="../../static/img/thomas.jpg" alt="Thomas Ruyant" />
-          <div style={{backgroundColor: 'rgba(0,0,0,0.5)'}} className="uk-position-cover" />
-          <div className="uk-overlay uk-position-center uk-flex uk-flex-column uk-flex-middle">
-            <div className="uk-light uk-flex uk-flex-column uk-flex-middle">
-              <h2 className="uk-text-center">Défie Thomas sur <span className="uk-text-bold" style={{color: '#FF5101'}}>Virtual Regatta</span> et <span className="uk-text-bold" style={{color: '#00B9EF'}}>gagne une navigation</span> sur le bateau LinkedOut&nbsp;!</h2>
-              <h3 className="uk-text-center">Top départ&nbsp;:&nbsp;<span className="uk-text-bold" style={{color: '#00B9EF'}}>{countdown}</span></h3>
+    <div>
+      <div id="modal-contest" uk-modal>
+        <div className="uk-modal-dialog uk-modal-body uk-background-primary">
+          <button
+            className="uk-modal-close-default"
+            type="button"
+            data-uk-close
+            style={{color: 'white'}}
+            aria-label="close"
+          />
+          <div className="uk-inline">
+            <img src="../../static/img/thomas.jpg" alt="Thomas Ruyant" />
+            <div style={{backgroundColor: 'rgba(0,0,0,0.5)'}} className="uk-position-cover" />
+            <div className="uk-overlay uk-position-center uk-flex uk-flex-column uk-flex-middle">
+              <div className="uk-light uk-flex uk-flex-column uk-flex-middle">
+                <h2 className="uk-text-center">Défie Thomas sur <span className="uk-text-bold" style={{color: '#FF5101'}}>Virtual Regatta</span> et <span className="uk-text-bold" style={{color: '#00B9EF'}}>gagne une navigation</span> sur le bateau LinkedOut&nbsp;!</h2>
+                <h3 className="uk-text-center">Top départ&nbsp;:&nbsp;<span className="uk-text-bold" style={{color: '#00B9EF'}}>{countdown}</span></h3>
+              </div>
+              <Button
+                href={EXTERNAL_LINKS.LKO_VG_CONTEST}
+                isExternal
+                newTab
+                style='secondary'
+                className="uk-margin-medium-top">
+                C&apos;est parti !{' '}<IconNoSSR name="chevron-right" />
+              </Button>
             </div>
-          <Button
-              href={EXTERNAL_LINKS.LKO_VG_CONTEST}
-              isExternal
-              newTab
-              style='secondary'
-              className="uk-margin-medium-top">
-              C&apos;est parti !{' '}<IconNoSSR name="chevron-right" />
-            </Button>
           </div>
         </div>
       </div>
