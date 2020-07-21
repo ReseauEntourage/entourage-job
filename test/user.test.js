756a8914bd8464e42a953be03079b1ebf8edb520
Arthur Brunck <arthur.brunck@neoxia.com>
2020-07-21 11:59:37 +0200

    feat: update jest config

--- test/user.test.js
+++ test/user.test.js
@@ -1,33 +1,32 @@
 import userFactory from './factories/userFactory';
 
 const {
-    startTestServer,
-    recreateTestDB,
-    stopTestServer,
-    createEntities,
+  startTestServer,
+  recreateTestDB,
+  stopTestServer,
+  createEntities,
 } = require('./helpers/helpers');
 
 const add = (a, b) => a + b;
 describe('Dummy', async () => {
-    it('Should add numbers', () => {
-        const res = add(5, 5);
-        expect(res).toBe(10);
-    })
+  test('Should add numbers', () => {
+    const res = add(5, 5);
+    expect(res).toBe(10);
+  })
 });
 
 describe('User', async () => {
-    beforeAll(async () => {
-        console.log('USER BEFORE ALL');
-        startTestServer();
-        recreateTestDB();
-        userFactory().then((user) => console.log('USER FACTORY', user));
-        console.log('CREATE MANY USERS', createEntities(userFactory(), 5));
-    });
-    afterAll(async () => {
-        stopTestServer();
-    });
-    describe('CRUD User', () => {
-        it('Should not create a user without being logged.', () => {
-        })
-    });
+  beforeAll(async () => {
+    console.log('USER BEFORE ALL');
+    await startTestServer();
+    await recreateTestDB();
+    await userFactory().then((user) => console.log('USER FACTORY', user));
+    // console.log('CREATE MANY USERS', createEntities(userFactory(), 5));
+  });
+  afterAll(async () => {
+    stopTestServer();
+  });
+  describe('CRUD User', () => {
+    test('Should not create a user without being logged.', () => {})
+  });
 });
