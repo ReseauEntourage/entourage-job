db6445b00b66284b8d96c739f3e9dd25122208fa
wa <a.welvart@gmail.com>
2020-07-27 09:51:33 +0200

    Add extension to git ignore

--- test/helpers/helpers.js
+++ test/helpers/helpers.js
@@ -12,109 +12,117 @@ let db;
  * Start a server according to .env variables
  */
 const startTestServer = async () => {
-  loadEnvironnementVariables();
-  app = server.prepare();
-  console.log('server port: ', process.env.PORT);
-  await server.start(process.env.PORT);
-  return app;
+    loadEnvironnementVariables();
+    app = server.prepare();
+    console.log('server port: ', process.env.PORT);
+    server.start(process.env.PORT);
+    return app;
 }
 
 /**
  * stop the server
  */
 const stopTestServer = async () => {
-  server.close()
+    server.close()
 }
 
 /**
  * Create a test database according to env variables
- *
+ * 
  * @returns the db connection
  */
 const recreateTestDB = async () => {
-  loadEnvironnementVariables();
-  console.log('DB URL: ', process.env.DATABASE_URL);
-  db = new Sequelize(
-    process.env.DATABASE_URL, {
-      logging: process.env.DEBUG_MODE ? console.log : false,
+    loadEnvironnementVariables();
+    console.log('DB URL: ', process.env.DATABASE_URL);
+    db = new Sequelize(
+        process.env.DATABASE_URL, {
+        logging: console.log,
     }
-  );
+    );
 
-  try {
-    await db.authenticate()
-  } catch (error) {
-    console.error('Impossible de se connecter à la base de données : ', error);
-  }
+    try {
+        await db.authenticate()
+    } catch (error) {
+        console.error('Impossible de se connecter à la base de données : ', error);
+    }
 
-  return db;
+    return db;
 };
 
 /**
  * Drops all the tables content
  */
 const resetTestDB = async () => {
-  db.drop({
-    force: true,
-    match: "/test$/",
-  });
+    db.drop({
+        force: true,
+        match: "/test$/",
+    });
 }
 
 /**
  * Create many entities using a factory
- *
+ * 
  * @param {function} factory an entity factory
  * @param {number} n the number of entities to create
  * @returns
  */
 const createEntities = async (factory, props = {}, n) => {
-  return Promise.all(Array(n).fill(0).map(() => factory(props)))
-    .catch((e) => console.error(e));
+    return Promise.all(Array(n).fill(0).map(() => factory(props)))
+        .catch((e) => console.error(e));
 }
 
 /**
  * Login a user
- *
- * @param {Object} user
+ * 
+ * @param {Object} user 
  * @returns {string} token
  */
 const getToken = async (user) => {
-  console.log('GET TOKEN USER MAIL: ', user.email, user.password);
-  const response = await request(app)
-    .post(`/api/v1/auth/login`)
-    .send({
-      email: user.email,
-      password: user.password,
-    });
+    console.log('GET TOKEN USER MAIL: ', user.email);
+    const response = await request(app)
+        .post(`/api/v1/auth/login`)
+        .send({
+            email: user.email,
+            password: user.password,
+        })
+        .expect(res => res.body != null);
 
-  if (response.status !== 200) {
-    console.error('Token couldn\'t be fetched', JSON.stringify(response.text));
-    throw new Error('Token couldn\'t be fectched');
-  }
+    if (response.status !== 200) {
+        console.error('Token couldn\'t be fetched', JSON.stringify(response.body));
+        throw new Error('Token couldn\'t be fectched');
+    }
 
-  return response.body.token;
+    return response.body.token;
 }
 
 /**
  * Create a user and get associated token
- *
+ * 
  * @param {Object} props user data
  */
 const getLoggedInUser = async (props = {}) => {
-  const user = await userFactory(props);
-  const token = await getToken({ ...user, password: props.password });
+    console.log('4');
 
-  return {
-    token,
-    user
-  };
+    const user = await userFactory(props);
+    console.log('5');
+
+    const token = await getToken(user);
+    console.log('6');
+
+    console.log(token, user);
+
+    return {
+        token,
+        user
+    };
 }
 
 
 module.exports = {
-  startTestServer,
-  stopTestServer,
-  recreateTestDB,
-  resetTestDB,
-  createEntities,
-  getLoggedInUser,
+    startTestServer,
+    stopTestServer,
+    recreateTestDB,
+    resetTestDB,
+    createEntities,
+    getLoggedInUser,
 }
