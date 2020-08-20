b04b6dea9a93b2917493569b5273e6e247f85ab5
wa <a.welvart@gmail.com>
2020-08-20 09:19:58 +0200

    Add C - / tests

--- test/opportunity.test.js
+++ test/opportunity.test.js
@@ -9,15 +9,13 @@ const {
     createLoggedInUser,
     associateCoachAndCandidat,
 } = require("./helpers");
-
 const { USER_ROLES } = require('../constants');
-
 const opportunityFactory = require("./factories/opportunityFactory");
 const userFactory = require('./factories/userFactory');
 
-describe('CRUD Opportunity', () => {
+describe('Opportunity', () => {
     let serverTest;
-    const route = 'api/v1/opportunity';
+    const route = '/api/v1/opportunity';
     let opportunitiesID;
     let loggedInAdmin;
     let loggedInCoach;
@@ -53,163 +51,165 @@ describe('CRUD Opportunity', () => {
         await resetTestDB();
         await stopTestServer();
     });
-    describe('C - Opportunity', () => {
-        describe('Anybody can create opportunities - /', () => {
-            it('Should return 200, if valid opportunity', async () => {
-                const opportunity = await opportunityFactory({}, false);
-                const response = await request(serverTest)
-                    .post(`${route}/`)
-                    .send(opportunity);
-                expect(response.status).toBe(200);
+    describe('CRUD Opportunity', () => {
+        describe('C - Opportunity', () => {
+            describe('Anybody can create opportunities - /', () => {
+                it('Should return 200, if valid opportunity', async () => {
+                    const opportunity = await opportunityFactory({}, false);
+                    const response = await request(serverTest)
+                        .post(`${route}/`)
+                        .send(opportunity);
+                    expect(response.status).toBe(200);
+                });
+                it('Should return 401, if invalid opportunity', async () => {
+                    const opportunity = await opportunityFactory({}, false);
+                    delete opportunity.recruiterMail;
+                    const response = await request(serverTest)
+                        .post(`${route}/`)
+                        .send(opportunity);
+                    expect(response.status).toBe(401);
+                });
             });
-            it('Should return 401, if invalid opportunity', async () => {
-                const opportunity = await opportunityFactory({}, false);
-                delete opportunity.recruiterMail;
-                const response = await request(serverTest)
-                    .post(`${route}/`)
-                    .send(opportunity);
-                expect(response.status).toBe(401);
-            });
-        });
-        describe('Add a user to an opportunity - /join', () => {
-            it('should return 200, if candidat had himself to an opportunity', async () => {
-
-            });
-            it(
-                'should return 200, if a coach had his associated candidat to an opportunity',
-                async () => {
+            describe.skip('Add a user to an opportunity - /join', () => {
+                it('should return 200, if candidat had himself to an opportunity', async () => {
 
                 });
-            it('should return 200, if admin had candidat to an opportunity', async () => {
+                it(
+                    'should return 200, if a coach had his associated candidat to an opportunity',
+                    async () => {
 
-            });
-            it('should return 401, if invalid opportunity id', async () => {
+                    });
+                it('should return 200, if admin had candidat to an opportunity', async () => {
 
-            });
-            it('should return 401, if candidat updates an other candidat', async () => {
+                });
+                it('should return 401, if invalid opportunity id', async () => {
 
-            });
-            it('should return 401, if a coach updates not associate candidat', async () => {
+                });
+                it('should return 401, if candidat updates an other candidat', async () => {
 
+                });
+                it('should return 401, if a coach updates not associate candidat', async () => {
+
+                });
             });
         });
+        describe.skip('R - Read 1 opportunity', () => {
+
+        });
+        describe.skip('R - Read Many opportunities', () => {
+            describe('Read all opportunities - /admin', () => {
+                it('Should return 200 and a list of opportunities, if logged in admin', () => {
+
+                });
+                it('Should return 401, if not logged in admin', () => {
+
+                });
+            });
+            describe('Read a user\'s private opportunities - /user/private/:id', () => {
+                it('should return 200, if candidat read his opportunities', async () => {
+
+                });
+                it(
+                    'should return 200, if a coach read his associated candidat opportunities',
+                    async () => {
+
+                    });
+                it(
+                    'should return 200, if a admin read his associated candidat opportunities',
+                    async () => {
+
+                    });
+                it('should return 401, if invalid user id', async () => {
+
+                });
+                it(
+                    'should return 401, if candidat reads an other candidat opportunities',
+                    async () => {
+
+                    });
+                it(
+                    'should return 401, if a coach read not associate candidat\'s opportunities',
+                    async () => {
+
+                    });
+            });
+            // TODO : check if this route also gets private opportunities
+            describe.skip('Read all user\'s opportunities - /user/all/:id', () => {
+                it('should return 200, if candidat read his opportunities', async () => {
+
+                });
+                it(
+                    'should return 200, if a coach read his associated candidat opportunities',
+                    async () => {
+
+                    });
+                it(
+                    'should return 200, if a admin read his associated candidat opportunities',
+                    async () => {
+
+                    });
+                it('should return 401, if invalid user id', async () => {
+
+                });
+                it(
+                    'should return 401, if candidat reads an other candidat opportunities',
+                    async () => {
+
+                    });
+                it(
+                    'should return 401, if a coach read not associate candidat\'s opportunities',
+                    async () => {
+
+                    });
+            });
+        });
+        describe.skip('U - Update 1', () => {
+            describe('Update an oppotunity - /', () => {
+                it('Should return 200, if admin updates an opportunity', async () => {
+
+                });
+                it('Should return 401, if no an admin', async () => {
+
+                });
+            });
+            describe('Update a user opportunity association - /join', () => {
+                it(
+                    'should return 200, if candidat updates his opportunities asociations',
+                    async () => {
+
+                    });
+                it(
+                    'should return 200, if a coach updates his associated candidat opportunities asociations', async () => {
+
+                    });
+                it(
+                    'should return 200, if a admin updates candidat opportunities asociations',
+                    async () => {
+
+                    });
+                it('should return 401, if invalid user id', async () => {
+
+                });
+                it(
+                    'should return 401, if candidat updates an other candidat opportunities asociations',
+                    async () => {
+
+                    });
+                it(
+                    'should return 401, if a coach updates not associate candidat\'s opportunities asociations',
+                    async () => {
+
+                    });
+            });
+        });
+        describe.skip('D - Delete 1', () => {
+            it('Should return 200, if admin', async () => {
+
+            });
+            it('Should return 401, if not admin', async () => {
+
+            });
+
+        })
     });
-    describe('R - Read 1 opportunity', () => {
-
-    });
-    describe('R - Read Many opportunities', () => {
-        describe('Read all opportunities - /admin', () => {
-            it('Should return 200 and a list of opportunities, if logged in admin', () => {
-
-            });
-            it('Should return 401, if not logged in admin', () => {
-
-            });
-        });
-        describe('Read a user\'s private opportunities - /user/private/:id', () => {
-            it('should return 200, if candidat read his opportunities', async () => {
-
-            });
-            it(
-                'should return 200, if a coach read his associated candidat opportunities',
-                async () => {
-
-                });
-            it(
-                'should return 200, if a admin read his associated candidat opportunities',
-                async () => {
-
-                });
-            it('should return 401, if invalid user id', async () => {
-
-            });
-            it(
-                'should return 401, if candidat reads an other candidat opportunities',
-                async () => {
-
-                });
-            it(
-                'should return 401, if a coach read not associate candidat\'s opportunities',
-                async () => {
-
-                });
-        });
-        // TODO : check if this route also gets private opportunities
-        describe('Read all user\'s opportunities - /user/all/:id', () => {
-            it('should return 200, if candidat read his opportunities', async () => {
-
-            });
-            it(
-                'should return 200, if a coach read his associated candidat opportunities',
-                async () => {
-
-                });
-            it(
-                'should return 200, if a admin read his associated candidat opportunities',
-                async () => {
-
-                });
-            it('should return 401, if invalid user id', async () => {
-
-            });
-            it(
-                'should return 401, if candidat reads an other candidat opportunities',
-                async () => {
-
-                });
-            it(
-                'should return 401, if a coach read not associate candidat\'s opportunities',
-                async () => {
-
-                });
-        });
-    });
-    describe('U - Update 1', () => {
-        describe('Update an oppotunity - /', () => {
-            it('Should return 200, if admin updates an opportunity', async () => {
-
-            });
-            it('Should return 401, if no an admin', async () => {
-
-            });
-        });
-        describe('Update a user opportunity association - /join', () => {
-            it(
-                'should return 200, if candidat updates his opportunities asociations',
-                async () => {
-
-                });
-            it(
-                'should return 200, if a coach updates his associated candidat opportunities asociations', async () => {
-
-                });
-            it(
-                'should return 200, if a admin updates candidat opportunities asociations',
-                async () => {
-
-                });
-            it('should return 401, if invalid user id', async () => {
-
-            });
-            it(
-                'should return 401, if candidat updates an other candidat opportunities asociations',
-                async () => {
-
-                });
-            it(
-                'should return 401, if a coach updates not associate candidat\'s opportunities asociations',
-                async () => {
-
-                });
-        });
-    });
-    describe('D - Delete 1', () => {
-        it('Should return 200, if admin', async () => {
-
-        });
-        it('Should return 401, if not admin', async () => {
-
-        });
-
-    })
 });
