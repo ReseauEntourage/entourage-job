bc15ec5f1d385b775cef827e949bc0b02c0e9576
wa <a.welvart@gmail.com>
2020-08-20 17:55:34 +0200

    Add jsDoc to opportunity foactory

--- test/factories/opportunityFactory.js
+++ test/factories/opportunityFactory.js
@@ -6,6 +6,24 @@ const {
     }
 } = require('../../backend/db/models');
 
+/**
+ * an oject which contains the data necessary
+ * to build an opportuity.
+ * 
+ * @param {Object} props opportunity data
+ * @param {string} props.title
+ * @param {boolean} props.isPublic
+ * @param {boolean} props.isValidated
+ * @param {boolean} props.isArchived
+ * @param {string} props.company
+ * @param {string} props.recruiterName
+ * @param {string} props.recruiterMail
+ * @param {string} props.recruiterPhone
+ * @param {string} props.date
+ * @param {string} props.location
+ * @param {string} props.decription
+ * @param {string} props.createdAt
+ */
 const generateOpportunity = async (props) => {
     const data = {
         title: faker.lorem.words(2),
@@ -26,7 +44,14 @@ const generateOpportunity = async (props) => {
         ...props,
     }
 }
-
+/**
+ * Create an opportunity and insert it in DB if desired
+ * 
+ * @param {*} props 
+ * @param {*} insertInDB 
+ * @returns opportunity inserted in DB (with id generated at insertion),
+ * @opional if no DB insertion returns the generated data (no id).
+ */
 const opportunityFactory = async (props = {}, insertInDB = true) => {
     let opportunityData = await generateOpportunity(props);
 
