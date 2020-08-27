5b864627681a202351f51f584e4272af02680aed
wa <a.welvart@gmail.com>
2020-08-27 17:11:54 +0200

    add helper

--- test/helpers/cvImage.helpers.js
+++ test/helpers/cvImage.helpers.js
@@ -3,19 +3,35 @@ const fakerStatic = require('faker');
 
 const path = require('path');
 
+/**
+ * Create a lorem image
+ */
 const createTestImage = () => {
-    // const image = fakerStatic.image.cats(600, 600);
+    const image = fakerStatic.image.cats(600, 600);
     const fileName = path.join(process.cwd(), 'imageTest.jpg')
 
-    // fs.writeFile(path, image, (error) => {
-    //     if (error) {
-    //         throw Error(error)
-    //     }
-    // })
+    fs.writeFile(path, image, (error) => {
+        if (error) {
+            throw Error(error)
+        }
+    })
 
     return fileName;
 }
 
+/**
+ * Delete a file
+ * 
+ * @param {string} pathToFile 
+ */
+const deleteTestImage = (pathToFile) => {
+    fs.unlink(pathToFile, (error) => {
+        if (error) throw error;
+        console.log(`Deleted: ${pathToFile}`)
+    });
+}
+
 module.exports = {
     createTestImage,
+    deleteTestImage,
 }
