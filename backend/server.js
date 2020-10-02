054ab12b11f7b404f1ef541b1a9a2b9a7c7a47ef
Emile Bex <emile.bex@gmail.com>
2020-10-02 14:56:04 +0200

    Revert "Add CORS to test CloudFront"
    This reverts commit 0549cd8389df7a2c2cfa00834642fdc47f77de4f.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const enforce = require('express-sslify');
-const cors = require('cors');
 
 const passport = require('./config/passport');
 
@@ -27,8 +26,6 @@ module.exports.prepare = () => {
   app.set('trust proxy', 1);
 
   app.use(express.json());
-  app.use(cors());
-
 
   // adding Passport
   app.use(passport.initialize());
