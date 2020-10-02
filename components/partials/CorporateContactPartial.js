8704db9981496397c9f7b4d833e121d8aac0725d
Stephane Ruhlmann <sj.ruhlmann@gmail.com>
2020-10-02 15:13:43 +0200

    Add corporate contact section to Entreprise page

---
+++ components/partials/CorporateContactPartial.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Button, Section } from '../utils';
+
+const CorporateContact = () => (
+  <Section style="muted">
+    <h2 className="uk-text-bold uk-text-center">
+      Une <span className="uk-text-primary">question ?</span>
+    </h2>
+    <p className="uk-text-center">
+      Pour tout renseignement, Florent se tient à votre disposition :
+    </p>
+    <div className="uk-flex uk-flex-center uk-flex-middle">
+      <img
+        style={{ width: '80px', borderRadius: '50%' }}
+        src="/static/img/florent.jpg"
+        alt="florent"
+      />
+      <a
+        href="mailto:florent@entourage.social"
+        target="_blank"
+        className="uk-text-bold uk-margin-small-left"
+        rel="noopener noreferrer"
+      >
+        <span uk-icon="mail" />
+        &nbsp;florent@entourage.social
+      </a>
+    </div>
+  </Section>
+);
+export default CorporateContact;
