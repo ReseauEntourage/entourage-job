32020169fcd5e4e5a492339ac341b001949a8bbd
Emile Bex <emile.bex@gmail.com>
2020-10-05 17:05:59 +0200

    Update or insert into new Airtable table for the offers

--- backend/controllers/Opportunity.js
+++ backend/controllers/Opportunity.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-restricted-globals */
 /* eslint-disable camelcase */
 
+const {airtable} = require('./airtable');
+
 const {
   models: {
     BusinessLine,
@@ -77,6 +79,24 @@ const INCLUDE_OPPORTUNITY_COMPLETE_ADMIN = [
   },
 ];
 
+const getAirtableOpportunityFields = (opportunity, candidat, businessLines) => {
+  return {
+    Id: opportunity.id,
+    Titre: opportunity.title,
+    Nom: opportunity.recruiterName,
+    Mail: opportunity.recruiterMail,
+    Téléphone: opportunity.recruiterPhone,
+    Entreprise: opportunity.company,
+    Localisation: opportunity.location,
+    Description: opportunity.description,
+    "Secteur d'activité": businessLines,
+    Publique: opportunity.isPublic,
+    Candidat: opportunity.isPublic ? '' : `${candidat.firstName} ${candidat.lastName}`,
+    Validé: opportunity.isValidated,
+    Archivé: opportunity.isArchived,
+  };
+}
+
 
 const createOpportunity = async (data) => {
   console.log(`createOpportunity - Création de l'opportunité`);
@@ -93,20 +113,50 @@ const createOpportunity = async (data) => {
         }).then((model) => model[0])
       )
     );
-    modelOpportunity.addBusinessLines(businessLines);
+    await modelOpportunity.addBusinessLines(businessLines);
   }
 
+  let candidat = null;
   if (data.candidatId) {
     console.log(`Etape 4 - Détermine le User à qui l'opportunité s'adresse`);
-    Opportunity_User.create({
+    await Opportunity_User.create({
       OpportunityId: modelOpportunity.id,
       UserId: data.candidatId, // to rename in userId
     });
+    candidat = await User.findByPk(data.candidatId);
   }
 
   console.log(`Etape finale - Reprendre l'opportunité complète à retourner`);
 
-  return cleanOpportunity(modelOpportunity);
+  const cleanedOpportunity = cleanOpportunity(modelOpportunity);
+
+  const fillTable = () => new Promise((res, rej) => {
+    airtable("Offres d'emploi v2").create(
+      [
+        {
+          fields: getAirtableOpportunityFields(cleanedOpportunity, candidat, data.businessLines),
+        },
+      ],
+      (err, records) => {
+        if (err) {
+          return rej(err);
+        }
+        return res();
+      }
+    );
+  });
+
+  try {
+    await fillTable();
+    console.log('Filled table with new offer.');
+  }
+  catch(err){
+    console.error(err);
+    console.log('Failed to fill table with new offer.');
+  }
+
+  return cleanedOpportunity;
+
 };
 
 const deleteOpportunity = (id) => {
@@ -292,7 +342,71 @@ const updateOpportunity = async (opportunity) => {
     });
   }
 
-  return getOpportunity(opportunity.id);
+  const finalOpportunity = await getOpportunity(opportunity.id);
+
+  const updateTable = () => new Promise((res, rej) => {
+
+    airtable("Offres d'emploi v2").select({
+      "filterByFormula": `"{Id}=${finalOpportunity.id}"`
+    }).firstPage((err, results) => {
+      if (err) {
+        return rej(err);
+      }
+
+      const candidat = (finalOpportunity.userOpportunity && finalOpportunity.userOpportunity.length > 0) ? finalOpportunity.userOpportunity[0].User : null;
+
+      const fields = getAirtableOpportunityFields(finalOpportunity, candidat, finalOpportunity.businessLines);
+
+      if(results.length === 0) {
+        airtable("Offres d'emploi v2").create(
+          [
+            {
+              fields,
+            },
+          ],
+          (error, records) => {
+            if (error) {
+              return rej(error);
+            }
+            return res();
+          }
+        );
+      }
+      else {
+        const record = results[0];
+
+        // grab the record id
+        // and then push an update to this record
+        const record_id = record.id;
+        airtable("Offres d'emploi v2").update(
+          [
+            {
+              id: record_id,
+              fields,
+            },
+          ],
+          (error, records) => {
+            if (error) {
+              return rej(error);
+            }
+            return res();
+          }
+        );
+      }
+
+    })
+  });
+
+  try {
+    await updateTable();
+    console.log('Updated table with modified offer.');
+  }
+  catch(err){
+    console.error(err);
+    console.log('Failed to update table with modified offer.');
+  }
+
+  return finalOpportunity;
 };
 
 module.exports = {
