e18902515eb5b1e6e4d025518ab15e6547df640d
Stephane Ruhlmann <sj.ruhlmann@gmail.com>
2020-10-08 15:09:06 +0200

    Make Hire CTA buttons BIGGER

--- components/partials/HireCTA.js
+++ components/partials/HireCTA.js
@@ -2,32 +2,32 @@ import React from 'react';
 import { Button, IconNoSSR, Section } from '../utils';
 import Grid from '../utils/Grid';
 
+import "./HireCTA.less"
+
 const HireCTA = () => {
   return (
     <Section container="small">
       <h2 className="uk-text-center uk-text-bold uk-text-primary">
         Vous souhaitez recruter&nbsp;?
       </h2>
-      <div className="uk-margin-top uk-text-center uk-text-bold">
+      <p className="uk-margin-medium-top uk-text-center uk-text-bold">
         Recherchez le candidat qui correspond à vos besoins et proposez-lui
         votre offre
-      </div>
+      </p>
       <Grid
         className="uk-margin-medium-top"
         childWidths={['1-2@m', 'auto']}
         center
         match
       >
-        <Button href="/entreprises/cvs" style="primary" size="large">
-          <span style={{ whiteSpace: 'nowrap' }}>
+        <Button href="/entreprises/cvs" style="primary" className="hireButton-xlarge">
             Recruter en Île-de-France et dans les Hauts-de-France
-          </span>
           <IconNoSSR name="chevron-right" />
         </Button>
         <Button
           href="/entreprises/recruter-hors-linkedout"
           style="primary"
-          size="large"
+          className="hireButton-xlarge"
         >
           Recruter ailleurs <IconNoSSR name="chevron-right" />
         </Button>
