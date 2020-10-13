10a3827376fe0899639db5be06b7180691fb65a9
Stephane Ruhlmann <sj.ruhlmann@gmail.com>
2020-10-13 10:48:02 +0200

    Reword page Aider

--- pages/aider.js
+++ pages/aider.js
@@ -3,8 +3,6 @@ import { Button, GridNoSSR, IconNoSSR, Section } from '../components/utils';
 import { NewsletterPartial, SharePartial } from '../components/partials';
 import { EXTERNAL_LINKS } from '../constants';
 import Layout from '../components/Layout';
-import HowItWorks from '../components/sections/HowItWorks';
-import MultipleCTA from '../components/partials/MultipleCTA';
 import ImageTitle from '../components/sections/ImageTitle';
 import ModalInterestLinkedOut from '../components/modals/ModalInterestLinkedOut';
 import { event } from '../lib/gtag';
@@ -21,19 +19,18 @@ const Aider = () => (
           Vous souhaitez <span className="uk-text-primary">aider&nbsp;?</span>
         </>
       }
-      text={"Il n'y a pas de petit coup de pouce, aidez à votre échelle\xa0!"}
     />
     <Section style="muted">
       <div className="uk-flex uk-flex-column uk-flex-middle uk-width-1-2@m uk-margin-auto">
         <h3 className="uk-text-bold uk-text-center">
-          Partagez votre réseau avec ceux qui n’en n’ont plus&nbsp;!
+          Partagez votre réseau avec ceux qui n’en n’ont pas
         </h3>
         <hr className="uk-divider-small uk-margin-remove-vertical" />
-        <div className="uk-text-center uk-flex-1 uk-margin-top uk-margin-bottom uk-padding-small uk-padding-remove-top uk-padding-remove-bottom">
-          Partager un CV sur vos réseaux donne une visibilité inédite à un
+        <p className="uk-text-center uk-padding-small uk-padding-remove-top">
+          Partager un CV dans vos réseaux donne une visibilité inédite à un
           candidat auprès de potentiels recruteurs et permet de générer des
           opportunités d’emploi. Votre partage peut tout changer&nbsp;!
-        </div>
+        </p>
         <div className="uk-flex-center uk-flex uk-flex-middle">
           <Button href="/candidats" style="secondary">
             Partager un CV <IconNoSSR name="chevron-right" />
@@ -41,44 +38,53 @@ const Aider = () => (
         </div>
       </div>
     </Section>
-    <Section container="small">
-      <MultipleCTA
-        data={[
-          {
-            title: 'Coachez une personne exclue vers l’emploi\xa0!',
-            text: (
-              <div>
-                Vous souhaitez donner de votre temps pour tisser une relation de
-                proximité avec un candidat et le coacher dans son retour à
-                l’emploi&nbsp;? Entourage vous forme à la mission de
-                bénévole-coach et vous donne les outils.
-              </div>
-            ),
-            button: {
-              label: 'Devenir bénévole-coach',
-              href: EXTERNAL_LINKS.ARTICLE_BC,
-              external: true,
-              onClick: () => event(TAGS.PAGE_AIDER_INSCRIPTION_COACH_CLIC),
-            },
-          },
-          {
-            title: 'Faire un don',
-            text: (
-              <div>
-                Vous souhaitez soutenir financièrement le projet LinkedOut et
-                participer à la construction d’une société plus inclusive
-              </div>
-            ),
-            button: {
-              label: 'Faire un don',
-              href: EXTERNAL_LINKS.DONATION,
-              external: true,
-              onClick: () => event(TAGS.PAGE_AIDER_DON_CLIC),
-            },
-          },
-        ]}
-        showHorizontalDividers
-      />
+    <Section style="default">
+      <div className="uk-flex uk-flex-column uk-flex-middle uk-width-1-2@m uk-margin-auto">
+        <h3 className="uk-text-bold uk-text-center">
+          Soutenez LinkedOut, faites un don
+        </h3>
+        <hr className="uk-divider-small uk-margin-remove-vertical" />
+        <p className="uk-text-center uk-padding-small uk-padding-remove-top">
+          Vous souhaitez soutenir financièrement le projet LinkedOut et
+          participer à la construction d’une société plus inclusive.
+        </p>
+        <div className="uk-flex-center uk-flex uk-flex-middle">
+          <Button
+            href={EXTERNAL_LINKS.DONATION}
+            isExternal
+            style="secondary"
+            onClick={() => event(TAGS.PAGE_AIDER_DON_CLIC)}
+          >
+            Devenir bénévole-coach&nbsp;
+            <IconNoSSR name="chevron-right" />
+          </Button>
+        </div>
+      </div>
+    </Section>
+    <Section style="muted">
+      <div className="uk-flex uk-flex-column uk-flex-middle uk-width-1-2@m uk-margin-auto">
+        <h3 className="uk-text-bold uk-text-center">
+          Coacher un candidat vers l’emploi
+        </h3>
+        <hr className="uk-divider-small uk-margin-remove-vertical" />
+        <p className="uk-text-center uk-padding-small uk-padding-remove-top">
+          Vous souhaitez donner de votre temps pour tisser une relation de
+          proximité avec un candidat et le coacher dans son retour à
+          l’emploi&nbsp;? Entourage vous forme à la mission de bénévole-coach et
+          vous donne les outils.
+        </p>
+        <div className="uk-flex-center uk-flex uk-flex-middle">
+          <Button
+            href={EXTERNAL_LINKS.ARTICLE_BC}
+            isExternal
+            style="secondary"
+            onClick={() => event(TAGS.PAGE_AIDER_INSCRIPTION_COACH_CLIC)}
+          >
+            Faire un don&nbsp;
+            <IconNoSSR name="chevron-right" />
+          </Button>
+        </div>
+      </div>
     </Section>
     <Section style="muted">
       <GridNoSSR gap="large" column>
