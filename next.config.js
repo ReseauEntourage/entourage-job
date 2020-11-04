e18ba324cea9a3d49cb8980e96aa4cbe325cb4e7
Emile Bex <emile.bex@gmail.com>
2020-11-04 12:24:11 +0100

    Test CDN + Next.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const withLess = require('@zeit/next-less');
 require('dotenv').config();
 const webpack = require('webpack');
 
+const dev = process.env.NODE_ENV !== 'production';
+
 module.exports = withLess(
   withCSS({
     /* config options here */
@@ -10,5 +12,6 @@ module.exports = withLess(
       config.plugins.push(new webpack.EnvironmentPlugin(process.env));
       return config;
     },
+    assetPrefix: !dev ? 'http://d280vsj7wg1qqw.cloudfront.net' : '',
   })
 );
