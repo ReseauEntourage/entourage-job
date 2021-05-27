d0c819e7235e5482ac0479d592a9d4a9bddb7af7
Emile Bex <emile.bex@gmail.com>
2021-05-27 16:45:58 +0200

    [EN-3701] fix(counter): fix model missing property

--- pages/cv/[url].js
+++ pages/cv/[url].js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'next/router';
 import {
