107ab0b9a7d387146d731022709c6c0a33e356a1
Emile Bex <emile.bex@gmail.com>
2021-10-01 12:05:26 +0200

    [EN-4135] feat(chat): install tawk widget

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,6 +16,21 @@ export default class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
+                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/6156c658d326717cb68436d4/1fgtfdifu';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+                })();
+              `,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
