a6f4126b2b4fdf5c0902602f64982a2ecaa650b5
Emile Bex <emile.bex@gmail.com>
2021-10-29 17:27:58 +0200

    [EN-3971] feat(pixels): add linkedin and facebook pixels

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
     return (
       <html lang="fr">
         <Head>
-          {/* Google Analytics  */}
+          {/* Google Analytics */}
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
@@ -25,7 +25,7 @@ export default class MyDocument extends Document {
               `,
             }}
           />
-          {/* Google Tag Manager  */}
+          {/* Google Tag Manager */}
           <script
             dangerouslySetInnerHTML={{
               __html: `
@@ -37,6 +37,59 @@ export default class MyDocument extends Document {
               `,
             }}
           />
+          {/* LinkedIn Insight Tag */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                _linkedin_partner_id = "${process.env.LINKEDIN_PARTNER_ID}";
+                window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+                window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+                (function(l) {
+                if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+                window.lintrk.q=[]}
+                var s = document.getElementsByTagName("script")[0];
+                var b = document.createElement("script");
+                b.type = "text/javascript";b.async = true;
+                b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                s.parentNode.insertBefore(b, s);})(window.lintrk);
+              `,
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              alt=""
+              src={`https://px.ads.linkedin.com/collect/?pid=${process.env.LINKEDIN_PARTNER_ID}&fmt=gif`}
+            />
+          </noscript>
+          {/* Facebook Pixel */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${process.env.FB_PIXEL_ID}');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src={`https://www.facebook.com/tr?id=${process.env.FB_PIXEL_ID}&ev=PageView&noscript=1`}
+              alt=""
+            />
+          </noscript>
         </Head>
         <body>
           <Main />
