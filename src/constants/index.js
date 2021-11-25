1c503d22da4cc6ba4ee9fd7b5a6434099b9f3618
Emile Bex <emile.bex@gmail.com>
2021-11-25 15:24:42 +0100

    [EN-4416] feat(TJV): changed banner

--- src/constants/index.js
+++ src/constants/index.js
@@ -233,9 +233,12 @@ const OFFER_ADMIN_FILTERS_DATA = [
 
 const CV_FILTERS_DATA = [
   {
-    key: 'hideEmployed',
+    key: 'employed',
     type: 'checkbox',
-    constants: [{ label: 'Masquer les candidats employés', value: true }],
+    constants: [
+      { label: "Recherche d'emploi", value: false },
+      { label: 'En emploi', value: true },
+    ],
     title: 'Masquer les candidats en emploi',
   },
   {
@@ -273,6 +276,9 @@ const OPPORTUNITY_FILTERS_DATA = [
   {
     key: 'department',
     constants: DEPARTMENTS_FILTERS,
+    priority: DEPARTMENTS_FILTERS.filter((dept) => {
+      return dept.zone !== ADMIN_ZONES.HZ;
+    }),
     title: 'Département',
   },
 ];
@@ -382,6 +388,8 @@ const EXTERNAL_LINKS = {
   LEGAL_MENTIONS:
     'https://docs.google.com/document/d/1a1IU9Y6qVDr4pvWJRE5XMVZ2fNhg0rhXMTL_lqY_f1M/pub',
   ARTICLE_BC: 'https://blog.entourage.social/2020/06/22/benevole-coach/',
+  ARTICLE_TJV:
+    'https://blog.linkedout.fr/2021/11/25/le-bateau-linkedout-vainqueur-de-la-transat-jacques-vabre/',
   CAMPUS_INCLUSION: 'https://campus-inclusion.fr',
   FRANCE_UNE_CHANCE:
     'https://lafrance-unechance.fr/carte-des-clubs-la-france-une-chance/',
@@ -407,9 +415,6 @@ const REDIS_KEYS = {
 };
 
 const STORAGE_KEYS = {
-  CV_FILTERS_PUBLIC: 'cv-filters-public',
-  CV_FILTERS_COMPANY: 'cv-filters-company',
-  MEMBERS_FILTERS: 'members-filters',
   ACCESS_TOKEN: 'access-token',
 };
 
