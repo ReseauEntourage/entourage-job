b4806fa49f2f86e81527846cd28b673bd3caa210
Emile Bex <emile.bex@gmail.com>
2021-11-29 18:40:39 +0100

    [EN-4239] feat(company page): add cta to airtable form on company page

--- src/components/partials/WhatItBringsToCompanies.js
+++ src/components/partials/WhatItBringsToCompanies.js
@@ -1,9 +1,46 @@
 import React from 'react';
 import Section from 'src/components/utils/Section';
+import Img from '../utils/Img';
+import { addPrefix } from '../../utils';
+import { Button, Grid } from '../utils';
+import { event } from '../../lib/gtag';
+import TAGS from '../../constants/tags';
+import ModalInterestLinkedOut from '../modals/ModalInterestLinkedOut';
 
 const WhatItBringsToCompanies = () => {
+  const content = (
+    <div className="uk-flex uk-flex-column uk-flex-middle uk-flex-center">
+      <div className="uk-light uk-flex uk-flex-column uk-flex-middle">
+        <h2 className="uk-text-bold uk-text-center uk-margin-bottom uk-margin-remove-top">
+          Une nouvelle promotion arrive à l&apos;automne 2021&nbsp;!
+        </h2>
+        <h3
+          className="uk-text-center uk-margin-remove-top"
+          style={{ color: 'white' }}
+        >
+          Plus de 160 candidats s&apos;apprêtent à se lancer dans la recherche
+          d&apos;une nouvelle expérience professionnelle&nbsp;!
+          <br />
+          Laissez-nous votre contact pour être tenus informés.
+        </h3>
+      </div>
+      <Grid middle column gap="collapse">
+        <Button
+          style="secondary"
+          toggle="target: #modal-interest-linkedOut"
+          onClick={() => {
+            return event(TAGS.PAGE_AIDER_CONTACT_RECRUTEUR_CLIC);
+          }}
+        >
+          Nous contacter
+        </Button>
+      </Grid>
+      <ModalInterestLinkedOut />
+    </div>
+  );
+
   return (
-    <Section id="whatItBrings" style="muted">
+    <Section id="whatItBrings" style="default">
       <div className="uk-flex uk-flex-column uk-flex-center uk-flex-middle">
         <h2 className="uk-text-bold uk-align-center uk-text-center uk-margin-medium-bottom uk-margin-remove-top">
           Ce que LinkedOut <span className="uk-text-primary">vous apporte</span>
@@ -36,6 +73,32 @@ const WhatItBringsToCompanies = () => {
             </h4>
           </li>
         </ul>
+      </div>
+      <div className="uk-margin-medium-top">
+        <div className="uk-inline uk-visible@m">
+          <Img
+            width="1500"
+            height="1000"
+            src="/static/img/candidats.jpg"
+            alt="Visages LinkedOut"
+          />
+          <div
+            style={{ backgroundColor: 'rgba(0,0,0,0.7)' }}
+            className="uk-position-cover"
+          />
+          <div className="uk-overlay uk-position-center uk-flex uk-flex-column uk-flex-center uk-flex-middle uk-padding-large">
+            {content}
+          </div>
+        </div>
+        <div
+          className="uk-hidden@m uk-flex uk-flex-column uk-flex-middle uk-padding-small uk-background-center-center uk-background-cover uk-background-blend-overlay"
+          style={{
+            backgroundImage: `url(${addPrefix('/static/img/candidats.jpg')})`,
+            backgroundColor: 'rgba(0,0,0,0.7)',
+          }}
+        >
+          {content}
+        </div>
       </div>
     </Section>
   );
