c794305682c36c6394c6a83daf3ea6c71831f43b
Emile Bex <emile.bex@gmail.com>
2022-06-23 17:59:50 +0200

    fix(bitly): add fallback if bitly limit is reached

--- src/utils/Mutating.js
+++ src/utils/Mutating.js
@@ -3,13 +3,20 @@ import { BitlyClient } from 'bitly';
 const bitly = new BitlyClient(process.env.BITLY_TOKEN);
 
 const getShortenedOfferURL = async (opportunityId, campaign) => {
-  const offerUrl = `${process.env.FRONT_URL}/backoffice/candidat/offres/${opportunityId}`;
-  const { link } = await bitly.shorten(
-    `${offerUrl.replace('localhost', '127.0.0.1')}${
-      campaign ? `?utm_source=SMS&utm_medium=SMS&utm_campaign=${campaign}` : ''
-    }`
-  );
-  return link;
+  try {
+    const offerUrl = `${process.env.FRONT_URL}/backoffice/candidat/offres/${opportunityId}`;
+    const { link } = await bitly.shorten(
+      `${offerUrl.replace('localhost', '127.0.0.1')}${
+        campaign
+          ? `?utm_source=SMS&utm_medium=SMS&utm_campaign=${campaign}`
+          : ''
+      }`
+    );
+    return link;
+  } catch (err) {
+    console.error(err);
+    return `${process.env.FRONT_URL}/login`;
+  }
 };
 
 export { getShortenedOfferURL };
