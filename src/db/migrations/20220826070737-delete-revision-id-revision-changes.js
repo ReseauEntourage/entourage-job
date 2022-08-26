eb008d13de6503a03394448d39982e7b284c08bd
Emile Bex <emile.bex@gmail.com>
2022-08-26 11:34:05 +0200

    feat(sequelize history): added missing migrations

---
+++ src/db/migrations/20220826070737-delete-revision-id-revision-changes.js
@@ -0,0 +1,11 @@
+export default {
+  up: (queryInterface) => {
+    return queryInterface.removeColumn('RevisionChanges', 'RevisionId');
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('RevisionChanges', 'RevisionId', {
+      allowNull: true,
+      type: Sequelize.UUID,
+    });
+  },
+};
